4ee4f9c9341faadcac20756daf01a22af6b2367c
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-20 11:29:49 -0600

    updated database file to reflect new database setup

--- database.sql
+++ database.sql
@@ -6,23 +6,26 @@ CREATE TABLE "person" (
     "password" VARCHAR (1000) NOT NULL
 );
 
-CREATE TABLE "userPreferences" (
-	"id" serial NOT NULL,
-	"person_id" int NOT NULL,
-	"type_name" varchar (255) NOT NULL,
-	"start_date" timestamp with time zone,
-	"end_date" timestamp with time zone,
-	"time_duration" TIME,
-	"days_out_of_the_week" varchar (255)
+CREATE TABLE "userpreferences" (
+    "id" serial NOT NULL,
+    "person_id" int NOT NULL,
+    "type_name" varchar (255) NOT NULL,
+    "start_date" timestamp with time zone,
+    "end_date" timestamp with time zone,
+    "time_duration" interval,
+    "days_out_of_the_week" varchar (255)
 );
 
 CREATE TABLE "events" (
-	"id" serial NOT NULL,
-	"person_id" int NOT NULL,
-	"type_name" varchar (255) NOT NULL,
-	"event_name" varchar (255) NOT NULL,
-	"start_date" timestamp with time zone,
-	"end_date" timestamp with time zone,
-	"time_duration" TIME,
-	"days_out_of_the_week" varchar (255)
+    "id" serial NOT NULL,
+    "person_id" int NOT NULL,
+    "type_name" varchar (255) NOT NULL,
+    "event_name" varchar (255) NOT NULL,
+    "start_date" timestamp with time zone,
+    "end_date" timestamp with time zone,
+    "time_duration" interval,
+    "days_out_of_the_week" varchar (255)
 );
+
+INSERT INTO userpreferences (person_id, type_name, start_date, end_date, time_duration, days_out_of_the_week) 
+VALUES (1, 'Fitness', '2004-10-19 10:23:54+02', '2004-10-19 13:23:54+02', '20 minutes', 'M, W, F');
