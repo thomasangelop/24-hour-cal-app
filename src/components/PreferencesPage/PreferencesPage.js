7d3b505ace5639ce624017a3433a78ce9ffb672a
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-21 09:48:35 -0600

    delete button moved to pref-page

--- src/components/PreferencesPage/PreferencesPage.js
+++ src/components/PreferencesPage/PreferencesPage.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import CreateNewP from './CreateNewP';
-import EditP from './EditP';
-import DeleteP from './DeleteP';
 import { connect } from 'react-redux';
-
-
 
 //import paper information from material ui
 import PropTypes from 'prop-types';
@@ -13,6 +9,9 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+
 
 // Form Dialogs inports
 import TextField from '@material-ui/core/TextField';
@@ -54,6 +53,9 @@ const styles = theme => ({
   textField: {
     padding: 15,
     margin: 10,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
   },
 });
 
@@ -126,6 +128,7 @@ class PreferencesPage extends React.Component {
     Create or delete a new preference type.
     </p>
     <CreateNewP />
+    {/* Paper card div */}
     <div>
       {this.props.reduxState.pref.map(preferences => (
           <Paper className={classes.root} elevation={1} key={preferences.id}>
@@ -150,164 +153,170 @@ class PreferencesPage extends React.Component {
              <Typography>
              <p>Days Out of the Week: {preferences.days_out_of_the_week}</p>
              </Typography>
-             {/* <EditP key={preferences.id} onChange={this.handleChangeFor}/> */}
              
+             {/* Edit button div below */}
              <div>
-      <MuiThemeProvider theme={theme}>
-        <Button variant="contained" color="secondary" style={{ cursor: 'pointer' }} className={classes.button} onClick={this.handleClick} onClick={this.handleClickOpen}>
-            Edit
-        </Button>
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">Edit Preference</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              To edit a preference, please adjust your info here.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Preference Type Name"
-              type="text"
-              fullWidth
-            />
-            <br />
-            <TextField
-             id="datetime-local"
-             label="Start Date"
-             type="datetime-local"
-             defaultValue="2018-11-24T12:30"
-             className={classes.textField}
-             InputLabelProps={{
-             shrink: true,
-             }}
-             />
-             <br />
-             <TextField
-             id="datetime-local"
-             label="End Date"
-             type="datetime-local"
-             defaultValue="2018-11-24T12:30"
-             className={classes.textField}
-             InputLabelProps={{
-             shrink: true,
-             }}
-             />
-             <br />
-             <TextField
-              id="filled-adornment-weight"
-              className={classes.margin}
-              className={classes.textField}
-              label="Duration (Hours)"
-              InputProps={{
-              endAdornment: (
-                <InputAdornment variant="filled" position="end">
-                  Hours
-                </InputAdornment>
-              ),
-              }}
-              />
-              <TextField
-              id="filled-adornment-weight"
-              className={classes.margin}
-              className={classes.textField}
-              label="Duration (Minutes)"
-              InputProps={{
-              endAdornment: (
-                <InputAdornment variant="filled" position="end">
-                  Minutes
-                </InputAdornment>
-              ),
-              }}
-              />
-              <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.monday}
-                 onChange={this.handleChange('monday')}
-                 value="monday"
-                 indeterminate/>}
-                label="Monday"
-             />
-             <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.tuesday}
-                 onChange={this.handleChange('tuesday')}
-                 value="tuesday"
-                 indeterminate/>}
-                label="Tuesday"
-             />
-             <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.wednesday}
-                 onChange={this.handleChange('wednesday')}
-                 value="wednesday"
-                 indeterminate/>}
-                label="Wednesday"
-             />
-             <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.thursday}
-                 onChange={this.handleChange('thursday')}
-                 value="thursday"
-                 indeterminate/>}
-                label="Thursday"
-             />
-             <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.friday}
-                 onChange={this.handleChange('friday')}
-                 value="friday"
-                 indeterminate/>}
-                label="Friday"
-             />
-             <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.saturday}
-                 onChange={this.handleChange('saturday')}
-                 value="saturday"
-                 indeterminate/>}
-                label="Saturday"
-             />
-             <br />
-             <FormControlLabel
-                control={
-                <Checkbox
-                checked={this.state.sunday}
-                 onChange={this.handleChange('sunday')}
-                 value="sunday"
-                 indeterminate/>}
-                label="Sunday"
-             />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              Save
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </MuiThemeProvider>
-    </div>
-
-             <DeleteP key={preferences.id}/>
+              <MuiThemeProvider theme={theme}>
+                <Button variant="contained" color="secondary" style={{ cursor: 'pointer' }} className={classes.button} onClick={this.handleClick} onClick={this.handleClickOpen}>
+                    Edit
+                </Button>
+                <Dialog
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                  aria-labelledby="form-dialog-title"
+                >
+                  <DialogTitle id="form-dialog-title">Edit Preference</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      To edit a preference, please adjust your info here.
+                    </DialogContentText>
+                    <TextField
+                      autoFocus
+                      margin="dense"
+                      id="name"
+                      label="Preference Type Name"
+                      type="text"
+                      fullWidth
+                    />
+                    <br />
+                    <TextField
+                    id="datetime-local"
+                    label="Start Date"
+                    type="datetime-local"
+                    defaultValue="2018-11-24T12:30"
+                    className={classes.textField}
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                    />
+                    <br />
+                    <TextField
+                    id="datetime-local"
+                    label="End Date"
+                    type="datetime-local"
+                    defaultValue="2018-11-24T12:30"
+                    className={classes.textField}
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                    />
+                    <br />
+                    <TextField
+                      id="filled-adornment-weight"
+                      className={classes.margin}
+                      className={classes.textField}
+                      label="Duration (Hours)"
+                      InputProps={{
+                      endAdornment: (
+                        <InputAdornment variant="filled" position="end">
+                          Hours
+                        </InputAdornment>
+                      ),
+                      }}
+                      />
+                      <TextField
+                      id="filled-adornment-weight"
+                      className={classes.margin}
+                      className={classes.textField}
+                      label="Duration (Minutes)"
+                      InputProps={{
+                      endAdornment: (
+                        <InputAdornment variant="filled" position="end">
+                          Minutes
+                        </InputAdornment>
+                      ),
+                      }}
+                      />
+                      <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.monday}
+                        onChange={this.handleChange('monday')}
+                        value="monday"
+                        indeterminate/>}
+                        label="Monday"
+                    />
+                    <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.tuesday}
+                        onChange={this.handleChange('tuesday')}
+                        value="tuesday"
+                        indeterminate/>}
+                        label="Tuesday"
+                    />
+                    <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.wednesday}
+                        onChange={this.handleChange('wednesday')}
+                        value="wednesday"
+                        indeterminate/>}
+                        label="Wednesday"
+                    />
+                    <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.thursday}
+                        onChange={this.handleChange('thursday')}
+                        value="thursday"
+                        indeterminate/>}
+                        label="Thursday"
+                    />
+                    <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.friday}
+                        onChange={this.handleChange('friday')}
+                        value="friday"
+                        indeterminate/>}
+                        label="Friday"
+                    />
+                    <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.saturday}
+                        onChange={this.handleChange('saturday')}
+                        value="saturday"
+                        indeterminate/>}
+                        label="Saturday"
+                    />
+                    <br />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                        checked={this.state.sunday}
+                        onChange={this.handleChange('sunday')}
+                        value="sunday"
+                        indeterminate/>}
+                        label="Sunday"
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                      Cancel
+                    </Button>
+                    <Button onClick={this.handleClose} color="primary">
+                      Save
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </MuiThemeProvider>
+              </div>
+            {/* Delete button div below */}
+            <div>
+              <MuiThemeProvider theme={theme}>
+                <IconButton className={classes.button} aria-label="Delete">
+                <DeleteIcon />
+                </IconButton>
+              </MuiThemeProvider>
+            </div>
           </Paper>
       )
       )}
