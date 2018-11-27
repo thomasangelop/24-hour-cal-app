03ee1e914ccf13f48ad940f6fc47573d1c0c6fda
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-27 13:33:39 -0600

    create new event is working

--- server/routes/newOneTimeEvent.router.js
+++ server/routes/newOneTimeEvent.router.js
@@ -2,19 +2,22 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-// Setup a POST route to add a new pref to the database
+// Setup a POST route to add a new event to the database
 router.post('/', (req, res) => {
-    const pref = req.body;
+    const event = req.body;
     const sqlText = `
     INSERT INTO user1events ("start", "end", "text", "color") 
     VALUES ($1, $2, $3, $4)`;
-        ('2018-11-28 12:00:00-00', 
-        '2018-11-28 13:00:00-00', 
-        'Text Test <div class="md-event-desc">NOT BOLD DESCRIPTION</div><div class="md-event-loc mbsc-txt-muted"><span class="mbsc-ic mbsc-ic-location"></span>Office</div>'
-        , 'ffffff')
-    pool.query(sqlText, [`asdfasdfasdf${pref.person_id}`, pref.type_name, pref.start_date, pref.end_date, pref.time_duration, pref.days_out_of_the_week])
+    pool.query(sqlText, [
+            `${event.startYear}-${event.startMonth}-${event.startDay} 
+            ${event.startHour}:${event.startMinute}:00-00 ${event.startAMPM}`, 
+            `${event.endYear}-${event.endMonth}-${event.endDay} 
+            ${event.endHour}:${event.endMinute}:00-00 ${event.endAMPM}`,
+            `<div class="mbsc-bold">${event.title}</div> <div class="md-event-desc">${event.description}</div><div class="md-event-loc mbsc-txt-muted">
+            <span class="mbsc-ic mbsc-ic-location"></span>${event.location}</div>`,
+            `${event.color}`])
         .then((result) => {
-            console.log(`Added to the database`, pref);
+            console.log(`Added to the database`, event);
             res.sendStatus(201);
         })
         .catch((error) => {
